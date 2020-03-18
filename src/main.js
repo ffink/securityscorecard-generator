f60ca39431aa55f52132e15402a7135f5702ed46
Frank Fink <ffink@github.com>
2020-03-18 16:58:42 -0700

    Create securityscorecard-generator.js

---
+++ src/main.js
@@ -0,0 +1,5 @@
+const run = require('./securityscorecard-generator.js');
+
+if (require.main === module) {
+  run();
+}
---
+++ src/securityscorecard-generator.js
@@ -0,0 +1,104 @@
+const core = require('@actions/core');
+const {GitHub, context} = require('@actions/github');
+const axios = require('axios').default;
+
+async function run() {
+  try {
+    // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
+    const github = new GitHub(process.env.GITHUB_TOKEN);
+
+    // Get owner, repo, and event from context of payload that triggered the action
+    const {owner, repo} = context.repo;
+
+    // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+    const command = '/ssc';
+
+    const event_type = context.eventName;
+    let issue_pr_number;
+    let body;
+
+    // Parse body of issue or PR to look for `/ssc <search_term>` based on event type
+    // Pull request event
+    // Webhook Documentation: https://developer.github.com/v3/activity/events/types/#pullrequestevent
+    if (event_type === 'pull_request') {
+      issue_pr_number = context.payload.pull_request.number;
+      body = context.payload.pull_request.body;
+
+      core.debug(`${event_type} event triggered action, pr_number: ${issue_pr_number}, body: ${body}`)
+      // Issues event
+      // Webhook Documentation: https://developer.github.com/v3/activity/events/types/#issuesevent
+    } else if (event_type === 'issues') {
+      issue_pr_number = context.payload.issue.number;
+      body = context.payload.issue.body;
+
+      core.debug(`${event_type} event triggered action, issue_number: ${issue_pr_number}, body: ${body}`)
+      // Issue comment event
+      // Webhook Documentation: https://developer.github.com/v3/activity/events/types/#issuecommentevent
+    } else {
+      issue_pr_number = context.payload.issue.number;
+      body = context.payload.comment.body;
+
+      core.debug(`${event_type} event triggered action, issue_or_pr_number: ${issue_pr_number}, body: ${body}`)
+    }
+
+    if (body.includes(command)) {
+      const index = body.lastIndexOf(command);
+      const query = body.substring(index + command.length).trim();
+      core.debug(`/ssc command found, query = ${query}`);
+
+      // Query SSC for a scorecard!
+      // API Documentation: https://platform.securityscorecard.io/docs/index.html
+      // eslint-disable-next-line no-await-in-loop
+      const headers = {
+        'Accept': 'application/json; charset=utf-8',
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${process.env.SSC_TOKEN}`,
+        'cache-control': 'no-cache',
+      };
+
+      const searchForScorecard = await axios.get(
+        `https://api.securityscorecard.io/companies/${query}`, {headers}
+      );
+
+      core.debug(`Successfully queried SecurityScorecard for ${query}`);
+
+      const {
+        name: companyName,
+        domain: companyDomain,
+        grade_url: companyGradeUrl,
+        industry: companyIndustry,
+        size: companySize,
+        score: companyScore,
+        grade: companyGrade,
+        last30day_score_change: companyLast30Change
+      } = searchForScorecard();
+
+      // Create a comment
+      // API Documentation: https://developer.github.com/v3/issues/comments/#create-a-comment
+      // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-issues-create-comment
+      // eslint-disable-next-line no-await-in-loop
+      await github.issues.createComment({
+        owner,
+        repo,
+        issue_number: issue_pr_number,
+        body: `![${companyGrade}](${companyGradeUrl})
+        | Attribute | Value |
+        |--|--|
+        | Company Name | ${companyName} |
+        | Company Domain | ${companyDomain} |
+        | Company Industry | ${companyIndustry} |
+        | Company Size | ${companySize} |
+        | Company Score | ${companyScore} |
+        | Company Grade | ${companyGrade} |
+        | 30-day Score Change | ${companyLast30Change}`
+      });
+      core.debug(`Successfully created comment on #: ${issue_pr_number}`);
+    } else {
+      core.debug(`/ssc command not found in body: ${body}, exiting`);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = run;
